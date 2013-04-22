b2708fb165e153d2caa0e32261f5df95b546eaa7
Abdullah Abdulaziz <abood320@gmail.com>
2013-04-22 21:54:24 +0300

    adding addPlayer & removePlayer functions

--- src/me/Sr4B/teams/Team.java
+++ src/me/Sr4B/teams/Team.java
@@ -1,5 +1,9 @@
 package me.Sr4B.teams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Color;
 import org.bukkit.entity.Player;
 
@@ -11,7 +15,7 @@ public class Team {
 	final String name;
 	final String id;
 	final String capacity;
-	final Player[] players;
+	Player[] players;
 	
 	public Team (Color color, String name, String id, String capacity,  Player[] players)
 	{
@@ -30,11 +34,15 @@ public class Team {
 	//adding a player to "this" Team, true if everything is ok, false if else
 	public boolean addPlayer(Player player)
 	{
+		players[players.length+1] = player;
 		return false;
 	}
 	//removing a player from "this" Team, true if everything is ok, false if else
 	public boolean removePlayer(Player player)
 	{
+		List<Player> list = new ArrayList<Player>(Arrays.asList(players));
+		list.removeAll(Arrays.asList(player));
+		players = list.toArray(players);
 		return false;
 	}
 	//writing the Team to the database, doesn't need any parameter
